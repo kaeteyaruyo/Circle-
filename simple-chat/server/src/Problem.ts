6989a6cbaeca1dc6110244251c49abfadae0ad05
Zames <z5254215560@gmail.com>
2019-06-10 17:47:29 +0800

    Add lobby socket

--- simple-chat/server/src/Problem.ts
+++ simple-chat/server/src/Problem.ts
@@ -3,15 +3,21 @@ import {getRangeRandom , getRandomProblem } from './Ramdom';
 import * as path from 'path';
 
 const problems = {
-    "keys" : ["basic","function","special"],
-    "basic" : ["<","==",">","%"],
-    "function" : ["isInteger(sqrt(x))","isFib(x)","isInterger(log(x))"],
-    "special" : ["x-2x+1","x+2x+1"]
-}
-
-function updateProblem(io,room,socket){
+    "keys": ["basic", "function", "special"],
+    "basic": ["<", "==", ">", "%"],
+    "function": ["isSquare(x)", "isPrime(x)", "inFibonacci(x)", "isPower2(x)"],
+    "special": ["x-2x+1", "x+2x+1"]
+};
+function updateProblem(gameRoom,roomName){
     let problem = getRandomProblem(problems);
+    gameRoom[roomName]["problem"] = problem;
+}
+function emitProblem(io,room,socket,problem){
     io.sockets.in(socket.room).emit('updateProblem', { problem:problem});
 }
-
-export {updateProblem}
+function initProblem(gameRoom,roomName){
+    if(typeof gameRoom[roomName]["problem"] === 'undefined'){
+        updateProblem(gameRoom,roomName);
+    }
+}
+export {updateProblem,emitProblem,initProblem}
