e9c6988dee82786ceb6f77557dc193cde6c6a42b
kaeteyaruyo <kaeteyaruyo@gmail.com>
2019-06-13 05:43:34 +0800

    Add in room checking in game room

--- static/js/game.js
+++ static/js/game.js
@@ -34,56 +34,71 @@ socket.emit('updateBullet', roomname, {
 });
 
 socket.on('startGame', (data) => {
-    if(data[username] !== undefined)
+    if(isInRoom(data.roomName) && data[username] !== undefined) {
         team = data[username].team;
+    }
 });
 
 socket.on('updateTimer', (data) => {
-    stage.updateTimer(`${ data.min }:${ data.sec.toString().padStart(2, '0') }`);
+    if(isInRoom(data.roomName)) {
+        stage.updateTimer(`${ data.min }:${ data.sec.toString().padStart(2, '0') }`);
+    }
 });
 
 socket.on('updateQuiz', (data) => {
-    stage.updateQuiz(data.problem);
-    cellUpdateQuiz(data.problem);
+    if(isInRoom(data.roomName)){
+        stage.updateQuiz(data.problem);
+        cellUpdateQuiz(data.problem);
+    }
 });
 
 socket.on('updateScore', (data) => {
-    if(data[username] !== undefined)
+    if(isInRoom(data.roomName) && data[username] !== undefined)
         stage.updateScore(data.score);
 });
 
 socket.on('updateCell', (data) => {
-    data.forEach(cellInfo => {
-        const cell = shapeLayer.findOne(`#cell${ cellInfo.index[0] }_${ cellInfo.index[1] }`);
-        if(cell){
-            cell.update(cellInfo.number, cellInfo.team);
-        }
-        else {
-            shapeLayer.add(createCell({
-                row: cellInfo.index[0],
-                column: cellInfo.index[1],
-                number: cellInfo.number,
-            }));
-        }
-    });
+    if(isInRoom(data.roomName)){
+        data.forEach(cellInfo => {
+            const cell = shapeLayer.findOne(`#cell${ cellInfo.index[0] }_${ cellInfo.index[1] }`);
+            if(cell){
+                cell.update(cellInfo.number, cellInfo.team);
+            }
+            else {
+                shapeLayer.add(createCell({
+                    row: cellInfo.index[0],
+                    column: cellInfo.index[1],
+                    number: cellInfo.number,
+                }));
+            }
+        });
+    }
 });
 
 socket.on('updateBullet', (data) => {
-    data.forEach(bulletInfo => {
-        shapeLayer.add(createBullet({
-            index: bulletInfo.index,
-            number: bulletInfo.bullet,
-            team: team,
-        }));
-    });
+    if(isInRoom(data.roomName)){
+        data.forEach(bulletInfo => {
+            shapeLayer.add(createBullet({
+                index: bulletInfo.index,
+                number: bulletInfo.bullet,
+                team: team,
+            }));
+        });
+    }
 });
 
 socket.on('stopGame', () => {
-    document.querySelector('.main__timesup').style.display = 'block';
-    setTimeout(() => {
-        window.location.href = `/summary/${ roomname }`;
-    }, 3000);
+    if(isInRoom(data.roomName)){
+        document.querySelector('.main__timesup').style.display = 'block';
+        setTimeout(() => {
+            window.location.href = `/summary/${ roomname }`;
+        }, 3000);
+    }
 });
 
 stage.fitStageIntoParentContainer();
 window.addEventListener('resize', stage.fitStageIntoParentContainer);
+
+function isInRoom(roomName){
+    return roomname === roomName;
+}
